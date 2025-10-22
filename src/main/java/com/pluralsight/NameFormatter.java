c2cf10115646de83d4703df9d499badb28919cb3
Stephen_A <ankustephen99@gmail.com>
2025-10-22 11:59:06 -0400

    Added static methods to format names in NameFormatter.java

--- src/main/java/com/pluralsight/NameFormatter.java
+++ src/main/java/com/pluralsight/NameFormatter.java
@@ -1,4 +1,22 @@
 package com.pluralsight;
 
 public class NameFormatter {
+    String Prefix;
+    String FirstName;
+    String MiddleName;
+    String LastName;
+    String Suffix;
+
+    public static String format(String firstName, String lastName) {
+        return lastName + ", " + firstName;
+    }
+
+    public static String format(String prefix, String firstName, String middleName, String lastName, String suffix) {
+        return lastName + ", " + prefix + " " + firstName + " " + middleName + ", " + suffix;
+    }
+
+    public static String format(String fullName) {
+
+    }
+
 }
