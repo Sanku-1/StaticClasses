791c7da9782c90b3b3399d2ffb54403144db47a8
Stephen_A <ankustephen99@gmail.com>
2025-10-22 13:59:08 -0400

    Implemented instructor's solution in order to use StringBuilder class

--- src/main/java/com/pluralsight/NameFormatter.java
+++ src/main/java/com/pluralsight/NameFormatter.java
@@ -13,8 +13,30 @@ public class NameFormatter {
         return lastName + ", " + firstName;
     }
 
+//    public static String format(String prefix, String firstName, String middleName, String lastName, String suffix) {
+//        return lastName + ", " + prefix + " " + firstName + " " + middleName + ", " + suffix;
+//    }
+
+    //Implementing string builder version shown by instructor
     public static String format(String prefix, String firstName, String middleName, String lastName, String suffix) {
-        return lastName + ", " + prefix + " " + firstName + " " + middleName + ", " + suffix;
+        StringBuilder formatBuilder = new StringBuilder();
+        formatBuilder.append(lastName).append(", ");
+
+        if (!prefix.isEmpty()) {
+            formatBuilder.append(prefix).append(" ");
+        }
+
+        formatBuilder.append(firstName);
+
+        if (!middleName.isEmpty()) {
+            formatBuilder.append(" ").append(middleName);
+        }
+
+        if (!suffix.isEmpty()) {
+            formatBuilder.append(", ").append(suffix);
+        }
+
+        return formatBuilder.toString();
     }
 
     public static String format(String fullName) {
